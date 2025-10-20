0fe362bf0cfa7b7e3d99ad8a2e0238d7f4a595ba
NikolinaLukic <nikolina.lukic@student.um.si>
2025-10-20 13:35:02 +0200

    Initial commit: simple calculator in C++

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    double result;
+
+    switch(op) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': 
+            if (b != 0) result = a / b;
+            else {
+                cout << "Error: division by zero!" << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Unknown operator!" << endl;
+            return 1;
+    }
+
+    cout << "Result: " << result << endl;
+    return 0;
+}
